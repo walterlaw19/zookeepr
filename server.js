855b05cf13b899d04722cf37486e0b69cf5c3444
SC2021 <walterlaw19@gmail.com>
2021-08-22 21:39:58 -0400

    fix PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.Port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json');
 
