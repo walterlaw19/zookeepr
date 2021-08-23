154a535dd3b8ac219361b2bccc531ef4c8895b4c
SC2021 <walterlaw19@gmail.com>
2021-08-23 19:52:33 -0400

    app.POST

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals.json');
 
 
@@ -24,6 +31,39 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray}, null, 2)
+  );
+  
+  console.log(body);
+  // our function's main code will go here!
+
+  // return finished code to post route for response
+  return animal;
+}
+
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -40,6 +80,25 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 });
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  }
+});
+
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -79,6 +138,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+
+
+  
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
